a69874fb349d58bebb4d1fbf295ba5b8bb3c0a8e
Kaitlyn Chen <kaitlynchen123@gmail.com>
2023-01-19 18:45:25 -0500

    Graphed all 9 dimensions

--- visualize.py
+++ visualize.py
@@ -7,7 +7,7 @@ import matplotlib.ticker as ticker
 from utils.data_loader import load_from_tsfile_to_dataframe
 
 
-def main():
+def graph_time_series(dim_name, units=""):
     df, _ = load_from_tsfile_to_dataframe(
         'data/BeijingPM25Quality_TRAIN.ts', True)
     df = df.rename(columns={"dim_0": "SO2", "dim_1": "NO2", "dim_2": "CO", "dim_3": "O3", "dim_4": "temperature",
@@ -16,43 +16,35 @@ def main():
     pd.set_option('display.max_columns', None)
     ym_list = []
     current_month = None
-    so2_monthly = []
-    so2_list = []
+    dim_monthly = []
+    dim_list = []
 
-    for date in df["SO2"]:
+    for date in df[dim_name]:
         month = date.index[0].strftime('%Y-%m')
         if current_month == None:
             current_month = month
             ym_list.append(month)
         elif month != current_month:
             current_month = month
-            so2 = np.mean(so2_monthly)
-            so2_list.append(so2)
-            so2_monthly = []
+            dim = np.mean(dim_monthly)
+            dim_list.append(dim)
+            dim_monthly = []
             ym_list.append(month)
 
-        so2_monthly.append(date.mean())
+        dim_monthly.append(date.mean())
 
-    so2_list.append(np.mean(so2_monthly))
-    so2_series = pd.Series(data=so2_list, index=ym_list)
+    dim_list.append(np.mean(dim_monthly))
+    dim_series = pd.Series(data=dim_list, index=ym_list)
 
-    # assert len(day_list) == len(so2_list)
-    # df["Date"] = day_list
-    # df["SO2"] = so2_list
-
-    # sns.set_style('darkgrid')
-    # sns.set(rc={'figure.figsize': (14, 8)})
-
-    # ax = sns.lineplot(data=df, x='Date', y='SO2', palette='viridis',
-    #                   legend='full', lw=3)
-    ax = sns.lineplot(data=so2_series)
-
+    ax = sns.lineplot(data=dim_series)
     ax.xaxis.set_major_locator(ticker.MultipleLocator(4))
-    plt.legend(bbox_to_anchor=(1, 1))
-    plt.ylabel('SO2 (µg/m3)')
+    dim_name = dim_name.capitalize()
+    plt.ylabel(dim_name + ' (' + units + ')')
     plt.xlabel('Year-Month')
-    plt.show()
+    plt.title(dim_name)
+    plt.savefig('graphs/' + dim_name + '.png')
+    plt.close()
 
 
 if __name__ == "__main__":
-    main()
+    graph_time_series("windspeed")
