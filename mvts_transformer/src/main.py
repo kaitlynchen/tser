081adaef3b76e0c3b071a394aad709665b2cb512
joshuafan <joshuayfan@hotmail.com>
2025-08-05 04:51:01 -0400

    Remove unneeded models

--- mvts_transformer/src/main.py
+++ mvts_transformer/src/main.py
@@ -73,34 +73,10 @@ def main(config):
         logger.info("Device index: {}".format(torch.cuda.current_device()))
 
     # Build data
-    if config["model"] is not None and config["model"] == "swin":
-        from models.ts_swin import model_factory
-    elif config["model"] is not None and config["model"] == "swin_pool":
-        from models.ts_swin_pool import model_factory
-    elif config["model"] is not None and config["model"] == "smooth":
-        from models.ts_smooth_transformer import model_factory
-    elif config["model"] is not None and config["model"] == "patch":
-        from models.patch_tst import model_factory
-    elif config["model"] is not None and config["model"] == "climax":
-        from models.ts_climax_base import model_factory
-    elif config["model"] is not None and config["model"] == "climax_smooth":
+    if config["model"] is not None and config["model"] == "climax_smooth":
         from models.ts_climax import model_factory
-    elif config["model"] is not None and config["model"] == "convit":
-        from models.climax_convit import model_factory
-    elif config["model"] is not None and config["model"] == "convit_smooth":
-        from models.climax_convit_smooth import model_factory
-    elif config["model"] is not None and config["model"] == "convit_2":
-        from models.climax_with_convit_blocks import model_factory
     elif config["model"] is not None and config["model"] == "local_cnn":
         from models.local_cnn import model_factory
-    elif config["model"] is not None and config["model"] == "climax_smooth_plot":
-        from models.ts_climax_timestep import model_factory
-    elif config["model"] is not None and config["model"] == "climax_smooth_pool":
-        from models.ts_climax_seqpool_smooth import model_factory
-    elif config["model"] is not None and config["model"] == "climax_max_pool":
-        from models.ts_climax_max_pool import model_factory
-    elif config["model"] is not None and config["model"] == "climax_seqpool":
-        from models.ts_climax_seqpool import model_factory
     else:
         from models.ts_transformer import model_factory
 
