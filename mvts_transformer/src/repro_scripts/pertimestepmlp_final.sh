663022d7789374d2d67b1c9a07cfcd7d3a29661b
kaitlynchen <61119947+kaitlynchen@users.noreply.github.com>
2026-02-18 17:34:41 -0800

    Revert "Add oversmoothing metrics"

--- mvts_transformer/src/repro_scripts/pertimestepmlp_final.sh
+++ mvts_transformer/src/repro_scripts/pertimestepmlp_final.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # ============================= USAGE ==============================
-# Tunes hyperparameters for Convit.
+# Runs best hyperparameters for per-timestep MLP.
 
-# ./repro_scripts/convit.sh <DATASET>
+# ./repro_scripts/pertimestepmlp_final.sh <DATASET>
 # (replace <DATASET> with one of: AppliancesEnergy BeijingPM10Quality BeijingPM25Quality BenzeneConcentration IEEEPPG LiveFuelMoistureContent)
 # Don't forget to change DATA_DIR and "ACTIVATE ENVIRONMENT" commands.
 
 # Results will be written to `output/${OUTPUT_FILE}.xls` (you can modify OUTPUT_FILE in this script).
-# Then, we test the best hyperparameters, and write result to `output/${OUTPUT_FILE}_TESTBEST.xls`
-# NOTE: The "test loss" column is MSE, take square root to get RMSE (except for MEAN/STD rows, which are RMSE)
+# NOTE: The "test loss" column is MSE, take square root to get RMSE.
 
 # ======== SLURM BOILERPLATE - Ignore if not using Slurm. =========
 # Request the full partition
@@ -39,71 +38,62 @@ conda activate tser
 
 # ======================= HYPERPARAMETERS =========================
 # Default settings
+BS=128
+PATCH=1
+STRIDE=1
 SPLIT=""
 
-# Batch size: 16 for AppliancesEnergy, otherwise 128
+# Dataset-specific overrides
 if [ "$DATA" = "AppliancesEnergy" ]; then
-    BS=16
+    BS=16  # Use smaller batch size for AppliancesEnergy, since there are only 95 train examples
 elif [ "$DATA" = "IEEEPPG" ]; then
-    BS=32
-else
-    BS=128
-fi
-if [ "$DATA" = "IEEEPPG" ]; then
-    PATCH=8
-    STRIDE=4
-elif [ "$DATA" = "LiveFuelMoistureContent" ]; then
     PATCH=4
     STRIDE=4
-else
-    PATCH=1
-    STRIDE=1
+elif [ "$DATA" = "LiveFuelMoistureContent" ]; then
+    PATCH=8
+    STRIDE=4
 fi
-if [ "$DATA" = "BenzeneConcentration" ]; then
-    D_MODEL=128
-    DIM_FEEDFORWARD=256
-elif [ "$DATA" = "AppliancesEnergy" ]; then
-    D_MODEL=128
-    DIM_FEEDFORWARD=512
+
+# Hyperparams
+if [ "$DATA" = "AppliancesEnergy" ]; then
+    LR=1e-2
+elif [ "$DATA" = "BeijingPM10Quality" ]; then
+    LR=1e-4
+elif [ "$DATA" = "BeijingPM25Quality" ]; then
+    LR=1e-4
+elif [ "$DATA" = "BenzeneConcentration" ]; then
+    LR=1e-3
 elif [ "$DATA" = "IEEEPPG" ]; then
-    D_MODEL=512
-    DIM_FEEDFORWARD=512
-else
-    D_MODEL=64
-    DIM_FEEDFORWARD=256
+    LR=1e-4
+elif [ "$DATA" = "LiveFuelMoistureContent" ]; then
+    LR=1e-4
 fi
 
-# OUTPUT FILE
-OUTPUT_FILE="${DATA}_CONVIT_TUNING"
-
-# TUNING
-for LR in 1e-2 1e-3 1e-4
+# Test per-timestep MLP
+for CONV_TYPE in per_timestep
 do
-    for CONVIT_SLOPE in 0.25
+    OUTPUT_FILE="${DATA}_${CONV_TYPE}_FINAL"
+    for POOL in seqpool_multihead
     do
-        for HEADS in 16 8
+        for WHERE_ABSPOS in before_pool_concat
         do
-            for SEED in 0
+            for SEED in 0 1 2
             do
-                # BASIC
-                PARAM_STR="LR=${LR}_SLOPE=${CONVIT_SLOPE}_HEADS=${HEADS}_SEED=${SEED}"
+                PARAM_STR="LR=${LR}_POOLSMOOTH=${LAM}_SEED=${SEED}"
                 python main.py --seed $SEED --name "${OUTPUT_FILE}_${PARAM_STR}" \
                     --records_file "output/${OUTPUT_FILE}.xls" \
                     --data_dir $DATA_DIR --data_class tsra \
-                    --pattern TRAIN --val_ratio 0.2 $SPLIT \
+                    --pattern TRAIN --val_pattern TEST \
                     --epochs 2000 --patience 200 \
                     --lr $LR --batch_size $BS \
-                    --num_layers 3 --num_heads $HEADS --d_model $D_MODEL --dim_feedforward $DIM_FEEDFORWARD \
-                    --optimizer RAdam --task regression \
-                    --plot_loss --plot_accuracy \
-                    --model climax_smooth --patch_length $PATCH --stride $STRIDE --smooth_attention --normalize_label \
-                    --pos_encoding learnable_sin_init --where_to_add_abspos start_add \
-                    --relative_pos_encoding convit --where_to_add_relpos after_gating --convit_slope $CONVIT_SLOPE \
-                    --pool seqpool_multihead
+                    --num_heads 16 --d_model 128 \
+                    --optimizer RAdam --task regression --normalize_label \
+                    --model local_cnn --conv_type $CONV_TYPE \
+                    --patch_length $PATCH --stride $STRIDE --smooth_attention \
+                    --pos_encoding learnable_sin_init --where_to_add_abspos $WHERE_ABSPOS \
+                    --pool $POOL \
+                    --plot_loss --plot_accuracy
             done
         done
     done
 done
-
-# TEST
-python test_best.py --records_file "output/${OUTPUT_FILE}.xls"
